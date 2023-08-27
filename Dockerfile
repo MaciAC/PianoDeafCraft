cbfad37b9da9ba45e0a0d8a448ccf0dd7902ddd9
MaciAC <macia.ac@gmail.com>
2023-08-27 19:41:38 +0200

    test output midi and MultiPitchKlapuri extractor

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt-get update
 RUN apt-get install -y python3-pip
 RUN pip3 install jupyter
 RUN pip3 install mir_eval
+RUN pip3 install mido
 
 EXPOSE 8888
 
--- pianodeafcraft/PianoCrafter.py
+++ pianodeafcraft/PianoCrafter.py
@@ -1,13 +1,11 @@
-# For embedding audio player
-import IPython
+from tempfile import TemporaryDirectory
+import mido
 
-# Plots
 import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = (15, 6)
 from mir_eval.sonify import pitch_contour
-
 import numpy
-
+from scipy.io.wavfile import read
 import essentia.standard as es
 
 
@@ -17,8 +15,9 @@ class PianoCrafter:
     
     def __init__(self, audiopath) -> None:
         self.audiopath = audiopath
+        self.temp_dir = TemporaryDirectory()
+        #self.audio, self.sr = read(self.audiopath)
         self.sr = 44100
-        # Load audio file.
         # It is recommended to apply equal-loudness filter for PredominantPitchMelodia.
         loader = es.EqloudLoader(filename=self.audiopath, sampleRate=self.sr)
         self.audio = loader()
@@ -28,24 +27,53 @@ class PianoCrafter:
     def extract_pitch(self):
         # Extract the pitch curve
         # PitchMelodia takes the entire audio signal as input (no frame-wise processing is required).
+        #pitch_extractor = es.PredominantPitchMelodia(frameSize=2048, hopSize=128)
+        pitch_extractor = es.MultiPitchKlapuri()
+        self.pitch_values = pitch_extractor(self.audio)
 
-        pitch_extractor = es.PredominantPitchMelodia(frameSize=2048, hopSize=128)
-        self.pitch_values, self.pitch_confidence = pitch_extractor(self.audio)
-
-    def plot_descriptors(self):
+    def plot_descriptors(pc):
         # Pitch is estimated on frames. Compute frame time positions.
-        self.pitch_times = numpy.linspace(0.0,len(self.audio)/self.sr,len(self.pitch_values) )
-
+        pc.pitch_times = numpy.linspace(0.0,len(pc.audio)/pc.sr,len(pc.pitch_values) )
+        mel1 = [p[0] for p in pc.pitch_values]
+        mel2 = []
+        for p in pc.pitch_values:
+            try:
+                mel2.append(p[1])
+            except:
+                mel2.append(0.0)
         # Plot the estimated pitch contour and confidence over time.
         f, axarr = plt.subplots(2, sharex=True)
-        axarr[0].plot(self.pitch_times, self.pitch_values)
-        axarr[0].set_title('estimated pitch [Hz]')
-        axarr[1].plot(self.pitch_times, self.pitch_confidence)
-        axarr[1].set_title('pitch confidence')
+        axarr[0].plot(pc.pitch_times, mel1)
+        axarr[0].set_title('estimated pitch 1 [Hz]')
+        axarr[1].plot(pc.pitch_times, mel2)
+        axarr[1].set_title('estimated pitch 2 [Hz]')
         plt.savefig('/data/descriptors.png')
         plt.show()
 
     def synth_melody(self):
         return pitch_contour(self.pitch_times, self.pitch_values, 44100).astype(numpy.float32)[:len(self.audio)]
 
+    def save_midi(self):
+        onsets, durations, notes = es.PitchContourSegmentation(hopSize=128)(self.pitch_values, self.audio)
 
+        PPQ = 96 # Pulses per quarter note.
+        BPM = 120 # Assuming a default tempo in Ableton to build a MIDI clip.
+        tempo = mido.bpm2tempo(BPM) # Microseconds per beat.
+
+        # Compute onsets and offsets for all MIDI notes in ticks.
+        # Relative tick positions start from time 0.
+        offsets = onsets + durations
+        silence_durations = list(onsets[1:] - offsets[:-1]) + [0]
+
+        mid = mido.MidiFile()
+        track = mido.MidiTrack()
+        mid.tracks.append(track)
+
+        for note, onset, duration, silence_duration in zip(list(notes), list(onsets), list(durations), silence_durations):
+            track.append(mido.Message('note_on', note=int(note), velocity=64,
+                                    time=int(mido.second2tick(duration, PPQ, tempo))))
+            track.append(mido.Message('note_off', note=int(note),
+                                    time=int(mido.second2tick(silence_duration, PPQ, tempo))))
+
+        midi_file = '/data/extracted_melody.mid'
+        mid.save(midi_file)
